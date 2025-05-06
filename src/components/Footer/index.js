ff883462733f85a76b7e8483c23dbf84f27536f1
Bárbara Azevedo <barbara.azevedo22@gmail.com>
2025-05-06 20:26:23 -0300

    Adjust Footer style

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,24 +3,31 @@ import './Footer.css';
 export const Footer = (props) => {
     return (
         <footer className="footer">
-            <div className="icons">
-                <a href="https://facebook.com" target="_blank">
-                    <img src="/images/fb.png" alt="Facebook" className="icon" />
-                </a>
-                <a href="https://linkedin.com" target="_blank">
-                    <img src="/images/tw.png" alt="LinkedIn" className="icon" />
-                </a>
-                <a href="https://instagram.com" target="_blank">
-                    <img src="/images/ig.png" alt="Instagram" className="icon" />
-                </a>
-            </div>
-            <div className="logo">
+            <section className="icons">
+                <ul>
+                    <li>
+                        <a href="https://facebook.com" target="_blank">
+                            <img src="/images/fb.png" alt="Facebook" className="icon" />
+                        </a>
+                    </li>    
+                    <li>
+                        <a href="https://linkedin.com" target="_blank">
+                            <img src="/images/tw.png" alt="LinkedIn" className="icon" />
+                        </a>
+                    </li> 
+                    <li>
+                        <a href="https://instagram.com" target="_blank">
+                            <img src="/images/ig.png" alt="Instagram" className="icon" />
+                        </a>
+                    </li> 
+                </ul>    
+            </section>
+            <section>
                 <img src="/images/logo.png" alt="Footer banner of organo page"></img>
-                
-            </div>
-            <div className='developer'>
+            </section>
+            <section>
                 <p>Developed by Bárbara Azevedo.</p>
-            </div>
+            </section>
         </footer>
     )
 }
